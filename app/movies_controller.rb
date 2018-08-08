d9c3798f7e0ee7adf1e8f8c19b30baf1825cb2b2
Monica  Karasek <mlkarasek@gmail.com>
2018-08-08 00:40:15 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -1,10 +1,3 @@
-# Replace the '__' in the below methods to to make the specs pass!
-# Each '__' corresponds to a single line of code you will need to write.
-# See the example below on the left and how it should look on the right.
-# def make_a_new_movie_instance    # def make_a_new_movie_instance
-#   movie = __                     #   movie = Movie.new
-# end                              # end
-
 def can_be_instantiated_and_then_saved
   movie = Movie.new
   movie.title = "This is a title."
@@ -25,10 +18,10 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
-
   Movie.create do |m|
     m.title = args[:title]
     m.release_date = args[:release_date]
+    m.save
   end
 end
 
@@ -57,7 +50,6 @@ def can_find_using_where_clause_and_be_sorted
 end
 
 def can_be_found_updated_and_saved
-  Movie.create(title: "Awesome Flick")
   movie = Movie.create(title: "Awesome Flick")
   movie = Movie.find_by(title: "Awesome Flick")
   movie.update(title: "Even Awesomer Flick")
@@ -71,12 +63,12 @@ def can_update_using_update_method
 end
 
 def can_update_multiple_items_at_once
-  # Change title of all movies to "A Movie"
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
   Movie.all.each do |movie|
-  movie.update(title: "A Movie")
+    movie.update(title: "A Movie")
+  end
 end
 
 def can_destroy_a_single_item
